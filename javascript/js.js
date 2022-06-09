e0bcf2551772787c28ff4b42b9b02156872da12d
Meeely <105783878+Meeely@users.noreply.github.com>
2022-06-09 09:48:05 +0200

    subido

---
+++ javascript/js.js
@@ -0,0 +1,11 @@
+
+function displayimg(box){
+    var floresimg= new Array(5);
+    floresimg[0]="";
+    floresimg[1]="";
+    floresimg[2]="";
+    floresimg[3]="";
+    floresimg[4]="";
+    document.getElementById('flores').scr=
+    floresimg[parseInt(box.value)];
+}
